678afd7d0f4f1390ff32a6e992a43cc6be024a06
s33dd <dsuhoverikov@gmail.com>
2022-09-13 03:14:12 +0200

    small rearragements

--- Handler/main.cpp
+++ Handler/main.cpp
@@ -43,34 +43,40 @@ int main(void) {
 		SocketClose(handlerSock);
 		return 0;
 	}
-
+	while(true) {
+		std::cout << "Trying to connect" << std::endl;
 #ifdef __linux__
-	socklen_t senderSize = sizeof(senderAddress);
-	if (connect(handlerSock, reinterpret_cast<sockaddr*>(&senderAddress), &senderSize) != 0) {
-		std::cout << "ERROR: Connection failed." << std::endl;
-		SocketClose(handlerSock);
-		return 0;
-	}
+		socklen_t senderSize = sizeof(senderAddress);
+		if (connect(handlerSock, reinterpret_cast<sockaddr*>(&senderAddress), &senderSize) != 0) {
+			std::cout << "ERROR: Connection failed." << std::endl;
+			sleep(1);
+		}
+		else {
+			break;
+		}
 #elif _WIN32
-	int senderSize = sizeof(senderAddress);
-	if (connect(handlerSock, reinterpret_cast<sockaddr*>(&senderAddress), senderSize)) {
-		std::cout << "ERROR: Connection failed." << std::endl;
-		SocketClose(handlerSock);
-		return 0;
-	}
+		int senderSize = sizeof(senderAddress);
+		if (connect(handlerSock, reinterpret_cast<sockaddr*>(&senderAddress), senderSize)) {
+			std::cout << "ERROR: Connection failed." << std::endl;
+			Sleep(1000);
+		}
+		else {
+			break;
+		}
 #endif
-	char data[BUFFER_SIZE];
+	}
 	std::cout << "Socket connected" << std::endl;
-	recv(handlerSock, data, BUFFER_SIZE, 0);
-	std::string strData = data;
-	int intData = std::stoi(strData);
-	if (strData.length() > 2 && intData % 32 == 0) {
-		std::cout << "Data was successfully received" << std::endl <<
-			"Data: " << intData << std::endl;
+	while(true) {
+		char data[BUFFER_SIZE];
+		recv(handlerSock, data, BUFFER_SIZE, 0);
+		std::string strData = data;
+		int intData = std::stoi(strData);
+		if (strData.length() > 2 && intData % 32 == 0) {
+			std::cout << "Data was successfully received" << std::endl <<
+				"Data: " << intData << std::endl;
+		}
+		else {
+			std::cout << "ERROR: Wrong data" << std::endl;
+		}
 	}
-	else {
-		std::cout << "ERROR: Wrong data" << std::endl;
-	}
-	SocketClose(handlerSock);
-	return 0;
 }
